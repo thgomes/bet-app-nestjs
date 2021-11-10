cadac80da6b16ed7ef6e5ecf9423e83304b51707
Thiago Gomes <thiagosiqgomes@gmail.com>
2021-11-10 15:27:27 -0300

    (chore): remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
@@ -23,7 +21,5 @@ import { ProfileModule } from './profile/profile.module';
     BetModule,
     ProfileModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
